a5052bad9d8adb4faa040eb1d0665992a1a3993d
Steven Galvez <stikgalvez@hotmail.com>
2019-03-28 22:11:58 -0400

    base7

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "3006622703";
+$password = "";
 $dbname = "stamps";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
