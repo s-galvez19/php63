f521edda7eac704101767d0bb1d548b62c49d0aa
Steven Galvez <stikgalvez@hotmail.com>
2019-03-28 21:08:34 -0400

    base3

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,6 +3,6 @@
 $servername = "localhost";
 $username = "root";
 $password = "3006622703";
-$dbname = "gallery.user";
+$dbname = "gallery";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
