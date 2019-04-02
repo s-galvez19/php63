8568c2a6f94b54f3de3036de90674a6f70a1ecd0
Steven Galvez <stikgalvez@hotmail.com>
2019-04-01 20:58:57 -0400

    arreglo1090

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 $server = 'localhost:3306';
 $username = 'root';
 $password = '3006622703';
-$database = 'database_php';
+$database = 'album';
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
 } catch (PDOException $e) {
